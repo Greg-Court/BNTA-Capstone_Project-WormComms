53c821655972432d5eae6cbe1ad8ed5325573ea2
J-emery <jamzwj@gmail.com>
2023-03-28 21:36:13 +0100

    added structure of log in page

--- worm-comms-frontend/src/Containers/MainPage.jsx
+++ worm-comms-frontend/src/Containers/MainPage.jsx
@@ -1,16 +1,32 @@
 import Contacts from "./Contacts";
 import MessageContainer from "./MessageContainer"
+import LoginPortal from "../Components/LoginPortal";
+import { useState } from "react";
 
 const MainPage = () => {
-    return (
-        <>
-        <div class="border-2 h-[5vh]">header</div>
-        <div class="flex">
-            <Contacts></Contacts>
-            <MessageContainer></MessageContainer>
-        </div>
-        </>
-    )
+
+    const [currentUser,setCurrentUser] = useState(null);
+
+    if(currentUser === null){
+        return(
+            <>
+            <div class="border-2 h-[5vh]">header</div>
+            <div class="flex justify-center items-center w-[100%] h-[95vh]">
+                <LoginPortal></LoginPortal>
+            </div>
+            </>
+        )
+    } else {
+        return (
+            <>
+            <div class="border-2 h-[5vh]">header</div>
+            <div class="flex">
+                <Contacts></Contacts>
+                <MessageContainer></MessageContainer>
+            </div>
+            </>
+        )
+    }
 }
 
 export default MainPage;
