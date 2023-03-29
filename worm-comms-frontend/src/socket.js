4399d7b68f229b3b19ab107d729134af4d5bfb1d
Greg <gregcourt10@gmail.com>
2023-03-29 14:30:40 +0100

    modified socket.js

--- worm-comms-frontend/src/socket.js
+++ worm-comms-frontend/src/socket.js
@@ -1,23 +1,24 @@
-import { useState, useEffect } from 'react';
-import SockJS from 'sockjs-client';
-import { Client } from '@stomp/stompjs';
+import { useState, useEffect } from "react";
+import SockJS from "sockjs-client";
+import { Client } from "@stomp/stompjs";
 
 // Custom hook to create and manage a WebSocket connection
 const useWebSocket = () => {
-  // Declare a state variable to store the WebSocket client
+  // The client state will store the STOMP client, which will be used to manage the WebSocket connection
   const [client, setClient] = useState(null);
 
   // Use the useEffect hook to create a WebSocket connection when the hook is used
   useEffect(() => {
-    // Create a new SockJS instance with the backend WebSocket URL
-    const socket = new SockJS('http://localhost:8080/ws');
-    
-    // Create a new STOMP client using the SockJS instance
-    const stompClient = new Client({
-      webSocketFactory: () => socket,
-    });
+    // Create a new SockJS instance URL points to the backend WebSocket URL
+    const socket = new SockJS("http://localhost:8080/ws");
 
-    // Update the WebSocket client state with the STOMP client
+    // new STOMP client is created using the SockJS instance
+    // webSocketFactory option is a function that returns the SockJS instance when called
+    // This setup allows the STOMP client to use the SockJS instance for communication
+    const stompClient = new Client({webSocketFactory: () => socket});
+
+    // setClient function is called to update the client state with the newly created STOMP client
+    // This makes the client available for use in the component.
     setClient(stompClient);
   }, []);
 
