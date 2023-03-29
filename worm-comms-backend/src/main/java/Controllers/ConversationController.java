52992287d00b6db44d8662ba0f64466084c8e11a
Greg <gregcourt10@gmail.com>
2023-03-29 11:06:40 +0100

    added full crud functionality

--- worm-comms-backend/src/main/java/Controllers/ConversationController.java
+++ worm-comms-backend/src/main/java/Controllers/ConversationController.java
@@ -3,6 +3,8 @@ package Controllers;
 import Models.Conversation;
 import Services.ConversationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,5 +24,16 @@ public class ConversationController {
     @PostMapping
     public Conversation createConversation(@RequestBody Conversation conversation) {
         return conversationService.saveConversation(conversation);
+    }
+
+    @PutMapping("/{id}")
+    public Conversation updateConversation(@PathVariable int id, @RequestBody Conversation conversation) {
+        return conversationService.updateConversation(id, conversation);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteConversation(@PathVariable int id) {
+        conversationService.deleteConversation(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
