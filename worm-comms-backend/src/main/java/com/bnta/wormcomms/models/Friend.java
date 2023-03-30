6a0167365630ba62405528db4ae6cdf591e4a77a
Greg <gregcourt10@gmail.com>
2023-03-30 10:03:18 +0100

    added fields

--- worm-comms-backend/src/main/java/com/bnta/wormcomms/models/Friend.java
+++ worm-comms-backend/src/main/java/com/bnta/wormcomms/models/Friend.java
@@ -8,10 +8,11 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name="friends")
 public class Friend {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+    @Column(name="name")
+    private String name;
 
     //here goes the link to the users
     @ManyToOne
@@ -24,11 +25,39 @@ public class Friend {
     @JsonIgnoreProperties({"friends"})
     private User user2;
 
+    @Column(name="created_by")
+    private int createdBy;
+
     public enum Status {
         PENDING,
         FRIEND,
         BLOCKED
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
     @Column
     @Enumerated(EnumType.STRING)
     private Status status;
@@ -39,13 +68,23 @@ public class Friend {
     @Column(name="updated_at")
     private LocalDateTime updatedAt;
 
-    public Friend(int id, User user1, User user2, Status status, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    public Friend(int id, User user1, User user2, Status status, LocalDateTime createdAt, LocalDateTime updatedAt, String name, int createdBy) {
         this.id = id;
         this.user1 = user1;
         this.user2 = user2;
         this.status = status;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
+        this.name = name;
+        this.createdBy = createdBy;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
     }
 
     public Friend() {
