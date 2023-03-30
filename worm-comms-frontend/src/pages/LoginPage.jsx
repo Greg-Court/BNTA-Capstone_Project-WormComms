558bc9060a38f8d3b4395e07c5243cd0131e00c4
Greg <gregcourt10@gmail.com>
2023-03-30 15:58:20 +0100

    fixed lots of bugzzzzz

--- worm-comms-frontend/src/pages/LoginPage.jsx
+++ worm-comms-frontend/src/pages/LoginPage.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import LoginHeader from '../Components/LoginHeader'
+import React from "react";
+import LoginHeader from "../Components/LoginHeader";
 
-import LoginPortal from '../Components/LoginPortal'
+import LoginPortal from "../Components/LoginPortal";
 
 export default function LoginPage() {
   return (
-    <>
+    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
         <LoginHeader
-            heading= "Login to WormComms"
-            paragraph="Dont have an account yet? "
-            linkName="Signup here"
-            linkUrl='/signup'
+          heading="Login to WormComms"
+          paragraph="Dont have an account yet? "
+          linkName="Signup here"
+          linkUrl="/signup"
         />
-        <LoginPortal/>
-    
-    </>
-  )
+        <LoginPortal />
+      </div>
+    </div>
+  );
 }
--- worm-comms-frontend/src/pages/SignUpPage.jsx
+++ worm-comms-frontend/src/pages/SignUpPage.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import LoginHeader from '../Components/LoginHeader'
-import SignUpPortal from '../Components/SignUpPortal'
+import React from "react";
+import LoginHeader from "../Components/LoginHeader";
+import SignUpPortal from "../Components/SignUpPortal";
 
 export default function SignUpPage() {
   return (
-   <>
-         <LoginHeader
-            heading= "Sign up to create an account"
-            paragraph="Already have an account? "
-            linkName="Login here"
-            linkUrl='/'
+    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="w-full space-y-8">
+        <LoginHeader
+          heading="Sign up to create an account"
+          paragraph="Already have an account? "
+          linkName="Login here"
+          linkUrl="/"
         />
-        <SignUpPortal/>
-   
-   </>
-  )
+        <SignUpPortal />
+      </div>
+    </div>
+  );
 }
