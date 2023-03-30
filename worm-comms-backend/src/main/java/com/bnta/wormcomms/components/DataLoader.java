218527efcdd35e58cbcfa829752233591e1a3bfc
J-emery <jamzwj@gmail,com>
2023-03-30 11:50:18 +0100

    not working :(

--- worm-comms-backend/src/main/java/com/bnta/wormcomms/components/DataLoader.java
+++ worm-comms-backend/src/main/java/com/bnta/wormcomms/components/DataLoader.java
@@ -1,14 +1,17 @@
 package com.bnta.wormcomms.components;
 
 import com.bnta.wormcomms.models.Chat;
+import com.bnta.wormcomms.models.Friend;
 import com.bnta.wormcomms.models.Message;
 import com.bnta.wormcomms.models.User;
 import com.bnta.wormcomms.repositories.ChatRepo;
+import com.bnta.wormcomms.repositories.FriendsRepo;
 import com.bnta.wormcomms.repositories.UserRepo;
 import com.bnta.wormcomms.repositories.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.data.repository.core.support.FragmentNotImplementedException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -19,6 +22,8 @@ public class DataLoader implements ApplicationRunner {
     MessageRepo messageRepo;
     @Autowired
     UserRepo userRepo;
+    @Autowired
+    FriendsRepo friendRepo;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
@@ -28,6 +33,10 @@ public class DataLoader implements ApplicationRunner {
         Chat chat = new Chat();
         Message message1 = new Message(james, chat, "hello");
         Message message2 = new Message(greg, chat, "hi");
+        Friend friend1 = new Friend(james,greg, Friend.Status.FRIEND,3);
+        Friend friend2 = new Friend(james,hansine, Friend.Status.PENDING,3);
+        friendRepo.save(friend1);
+        friendRepo.save(friend2);
         userRepo.save(greg);
         userRepo.save(hansine);
         userRepo.save(james);
