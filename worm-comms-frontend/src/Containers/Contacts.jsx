6952435fd4516d859fdb5e9ed1785fc5fe0834b8
J-emery <jamzwj@gmail.com>
2023-03-30 16:36:45 +0100

    friend drop down

--- worm-comms-frontend/src/Containers/Contacts.jsx
+++ worm-comms-frontend/src/Containers/Contacts.jsx
@@ -1,10 +1,13 @@
 import { useEffect } from "react"
 import { useCurrentUser } from "../UserContext";
 import Friend from "../Components/Friend";
+import { useState } from "react";
 
 const Contacts = () => {
 
     const { currentUser, setCurrentUser } = useCurrentUser();
+
+    const [newChat, setNewChat] = useState(null);
 
     const friends = currentUser.friends.map((friend, index) => {
         console.log(friend.user2)
@@ -13,12 +16,29 @@ const Contacts = () => {
         )
     })
 
+    const updateNewChat = (e) => {
+        
+        setNewChat(currentUser.friends.filter((friend) => {return e.target.value=== friend.user2.username})[0].user2)
+    }
+
+    const createNewChat = () => {
+
+    }
+
 
     return (
-        <ul className="min-w-[15%] h-[95vh] border-2">
-            <li>Friends:</li>
-            {friends}
-        </ul>
+        <div className="min-w-[15%] h-[95vh] border-2">
+            <div className="w-[100%]">
+            <select 
+            className="w-[80%]" 
+            onChange={updateNewChat}
+            value={newChat}>
+                <option className="h-max-5vh">New Chat:</option>
+                {friends}
+            </select>
+            <button onClick={createNewChat} className="w-[20%]"> + </button>
+            </div>
+        </div>
     )
 }
 
