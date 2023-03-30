ba2185cb75658326ee72f49229aa0c9061fa274c
J-emery <jamzwj@gmail.com>
2023-03-30 10:05:00 +0100

    added funtion to textarea

--- worm-comms-frontend/src/Components/TextArea.jsx
+++ worm-comms-frontend/src/Components/TextArea.jsx
@@ -1,40 +1,61 @@
 import { useCurrentUser } from '../UserContext';
+import { useState } from 'react';
 
-const TextArea = ({stompClient}) => {
-    const {currentUser, setCurrentUser} = useCurrentUser();
+const TextArea = ({ stompClient }) => {
+    const { currentUser, setCurrentUser } = useCurrentUser();
 
-    const message = {
-        app_user: currentUser,
-        conversation: {id:1},
-        content: "Hello World"
+    const [message, setMessage] = useState({
+        sender_id: currentUser,
+        chat: { id: 1 },
+        content: ""
+    })
+
+    const updateMessage = (e) => {
+        setMessage({
+            sender_id: currentUser,
+            chat: { id: 1 },
+            content: e.target.value
+        })
     }
 
     const sendMessage = () => {
-        console.log(stompClient)
-        stompClient.send("/app/user", {}, JSON.stringify(message));
+        if (message.content !== "") {
+            console.log(message)
+            stompClient.send("/app/user", {}, JSON.stringify(message));
+            setMessage({
+                sender_id: currentUser,
+                chat: { id: 1 },
+                content: ""
+            })
+        }
     }
 
 
     // useEffect(() => {
-        //     if (stompClient) {
-          
-        //         // Subscribe to the /topic/test destination to receive messages from the backend
-        //         stompClient.subscribe("/topic/test", (response) => {
-        //           const message = JSON.parse(response.body);
-        //           console.log("Message from backend:", message);
-        //         });
-          
-        //         // Send a test message to the backend
-        //         stompClient.send("/app/test", {}, JSON.stringify("Hello from frontend!"));
-        //       });
-        //     }
-        //   }, [stompClient]);
-        
+    //     if (stompClient) {
+
+    //         // Subscribe to the /topic/test destination to receive messages from the backend
+    //         stompClient.subscribe("/topic/test", (response) => {
+    //           const message = JSON.parse(response.body);
+    //           console.log("Message from backend:", message);
+    //         });
+
+    //         // Send a test message to the backend
+    //         stompClient.send("/app/test", {}, JSON.stringify("Hello from frontend!"));
+    //       });
+    //     }
+    //   }, [stompClient]);
+
 
     return (
         <div className="flex bottom">
-            <textarea className="border-2 w-5/6"
-                id="messageInput" name="messageInput" placeholder="Type your message here"></textarea>
+            <textarea
+                className="border-2 w-5/6"
+                id="messageInput"
+                name="messageInput"
+                placeholder="Type your message here"
+                value={message.content}
+                onChange={updateMessage}></textarea>
             <button onClick={sendMessage} className="border-2 w-1/12">Send</button>
             <button className="border-2 w-1/12">Toggle ChatGPT</button>
         </div>
