f6a10861fc117133392956bf0e21a78ddeedb765
hansine <hansi.marshall@gmail.com>
2023-03-30 15:26:43 +0100

    react router login problems

--- worm-comms-frontend/src/App.jsx
+++ worm-comms-frontend/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
 
   return (
     <>
+    <ChatProvider>
     <UserProvider>
       <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
@@ -29,6 +30,7 @@ function App() {
       </div>
       </div>
       </UserProvider>
+      </ChatProvider>
     </>
   );
 
