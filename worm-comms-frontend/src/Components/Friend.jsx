efb516845f52262ce51c7b9334f78eb9f2331954
J-emery <jamzwj@gmail,com>
2023-03-30 13:01:22 +0100

    some stuff I don't remember

---
+++ worm-comms-frontend/src/Components/Friend.jsx
@@ -0,0 +1,8 @@
+const Friend = ({friend,index}) => {
+
+    return(
+        <li>{friend.user2.username}</li>
+    )
+}
+
+export default Friend
--- worm-comms-frontend/src/Containers/Contacts.jsx
+++ worm-comms-frontend/src/Containers/Contacts.jsx
@@ -1,11 +1,23 @@
+import { useEffect } from "react"
+import { useCurrentUser } from "../UserContext";
+import Friend from "../Components/Friend";
+
 const Contacts = () => {
 
-    return(
+    const { currentUser, setCurrentUser } = useCurrentUser();
+
+    const friends = currentUser.friends.map((friend, index) => {
+        console.log(friend.user2)
+        return (
+            <Friend friend={friend} key={index}></Friend>
+        )
+    })
+
+
+    return (
         <ul className="min-w-[15%] h-[95vh] border-2">
-            <li>Chats:</li>
-            <li>James</li>
-            <li>Greg</li>
-            <li>Hansine</li>
+            <li>Friends:</li>
+            {friends}
         </ul>
     )
 }
