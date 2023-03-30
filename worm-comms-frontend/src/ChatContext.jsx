3e999f290780e3ae9bc0be240f96195d1c7af2cc
J-emery <jamzwj@gmail.com>
2023-03-30 14:44:44 +0100

    add thing

---
+++ worm-comms-frontend/src/ChatContext.jsx
@@ -0,0 +1,19 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const ChatContext = createContext();
+
+export const useCurrentChat = () => {
+  return useContext(ChatContext);
+}
+
+export const ChatProvider = ({children}) => {
+    const [currentChat, setCurrentChat] = useState(null);
+
+    return (
+        <ChatContext.Provider value={{currentChat, setCurrentChat}}>
+            {children}
+        </ChatContext.Provider>
+    )
+}
+
+export default UserContext;
