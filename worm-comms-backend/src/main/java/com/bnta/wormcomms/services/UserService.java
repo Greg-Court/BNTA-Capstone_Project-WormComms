e60c36c078ed1dddc6158f33e5026fa20004702f
Greg <gregcourt10@gmail.com>
2023-04-01 10:22:32 +0100

    added DTOs for Users and Friends (to transfer user data securely

--- worm-comms-backend/src/main/java/com/bnta/wormcomms/services/UserService.java
+++ worm-comms-backend/src/main/java/com/bnta/wormcomms/services/UserService.java
@@ -1,11 +1,14 @@
 package com.bnta.wormcomms.services;
 
 import com.bnta.wormcomms.models.Chat;
+import com.bnta.wormcomms.models.Friend;
+import com.bnta.wormcomms.models.FriendDTO;
 import com.bnta.wormcomms.models.User;
 import com.bnta.wormcomms.repositories.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -34,6 +37,18 @@ public class UserService {
         }
         throw new NoSuchElementException("User not found");
     }
+
+    public List<FriendDTO> getFriendsForUser(int userId) {
+        Optional<User> user = userRepository.findById(userId);
+        List<Friend> friends = user.get().getFriends();
+        List<FriendDTO> friendDTOs = new ArrayList<>();
+        for (Friend friend : friends) {
+            FriendDTO friendDTO = new FriendDTO(friend);
+            friendDTOs.add(friendDTO);
+        }
+        return friendDTOs;
+    }
+
 
     public void deleteUser(int id) {
         userRepository.deleteById(id);
