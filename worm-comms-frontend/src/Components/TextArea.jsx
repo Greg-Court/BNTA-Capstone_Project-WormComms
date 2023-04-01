777b4526fe584a9b87784979de9b80497e4dcdd4
Greg <gregcourt10@gmail.com>
2023-04-01 09:09:49 +0100

    futher styling tweaks and introduction of AutoreplyTM

--- worm-comms-frontend/src/Components/TextArea.jsx
+++ worm-comms-frontend/src/Components/TextArea.jsx
@@ -1,68 +1,81 @@
-import { useCurrentUser } from '../UserContext';
-import { useState } from 'react';
-import { useCurrentChat } from '../ChatContext';
-import { BsChevronDoubleLeft } from 'react-icons/bs';
+import { useCurrentUser } from "../UserContext";
+import { useState } from "react";
+import { useCurrentChat } from "../ChatContext";
+import { BsChevronDoubleLeft } from "react-icons/bs";
 
 const TextArea = ({ stompClient }) => {
-    const { currentUser, setCurrentUser } = useCurrentUser();
-    const { currentChat, setCurrentChat } = useCurrentChat();
+  const { currentUser, setCurrentUser } = useCurrentUser();
+  const { currentChat, setCurrentChat } = useCurrentChat();
+    const [isSelected, setIsSelected] = useState(false);
 
-    const [message, setMessage] = useState({
-        sender: currentUser,
-        chat: { id: 1 },
-        content: ""
-    })
-
-    const updateMessage = (e) => {
-        setMessage({
-            sender: currentUser,
-            chat: { id: currentChat.id },
-            content: e.target.value
-        })
-    }
-
-    const sendMessage = () => {
-        if (message.content !== "") {
-            const messageRequest = {
-                senderId: currentUser.id,
-                chatId: currentChat.id,
-                content: message.content
-            };
-            stompClient.send("/app/user", {}, JSON.stringify(messageRequest));
-            //console.log(JSON.stringify(messageRequest));
-            setMessage({
-                sender: currentUser,
-                chat: { id: currentChat.id },
-                content: ""
-            });
-            // console.log(currentUser);
-        }
+    const handleClick = () => {
+      setIsSelected(!isSelected);
     };
 
-    //sends message on enter
-    const handleKeyDown = (e) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            sendMessage();
-        }
+  const [message, setMessage] = useState({
+    sender: currentUser,
+    chat: { id: 1 },
+    content: "",
+  });
+
+  const updateMessage = (e) => {
+    setMessage({
+      sender: currentUser,
+      chat: { id: currentChat.id },
+      content: e.target.value,
+    });
+  };
+
+  const sendMessage = () => {
+    if (message.content !== "") {
+      const messageRequest = {
+        senderId: currentUser.id,
+        chatId: currentChat.id,
+        content: message.content,
+      };
+      stompClient.send("/app/user", {}, JSON.stringify(messageRequest));
+      //console.log(JSON.stringify(messageRequest));
+      setMessage({
+        sender: currentUser,
+        chat: { id: currentChat.id },
+        content: "",
+      });
+      // console.log(currentUser);
     }
+  };
 
+  //sends message on enter
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      sendMessage();
+    }
+  };
 
-    return (
-        <div className="flex border-t-2 border-gray-300">
-            <textarea
-                className="w-5/6"
-                id="messageInput"
-                name="messageInput"
-                placeholder="Type your message here"
-                value={message.content}
-                onKeyDown={handleKeyDown}
-                onChange={updateMessage}></textarea>
-            <button onClick={sendMessage} className="border-l-2 w-1/12">Send</button>
-            <button className="border-l-2 w-1/12">Toggle ChatGPT</button>
-        </div>
-    )
-}
+  return (
+    <div className="flex border-t-2 border-gray-300">
+      <textarea
+        className="w-5/6"
+        id="messageInput"
+        name="messageInput"
+        placeholder="Type your message here"
+        value={message.content}
+        onKeyDown={handleKeyDown}
+        onChange={updateMessage}
+      ></textarea>
+      <button onClick={sendMessage} className="border-l-2 w-1/12">
+        Send
+      </button>
+      <button
+        className={`border-l-2 w-1/12 ${
+          isSelected ? "bg-blue-500 text-white" : ""
+        }`}
+        onClick={handleClick}
+      >
+        Autoreply™
+      </button>
+    </div>
+  );
+};
 
 export default TextArea;
-
