643b2605dbd90cbeff2a03739201184fad5844ec
Greg <gregcourt10@gmail.com>
2023-04-01 11:02:33 +0100

    further DTO tweaks to ensure enough information is being returned

--- worm-comms-backend/src/main/java/com/bnta/wormcomms/models/FriendDTO.java
+++ worm-comms-backend/src/main/java/com/bnta/wormcomms/models/FriendDTO.java
@@ -3,17 +3,19 @@ package com.bnta.wormcomms.models;
 public class FriendDTO {
     private int user1Id;
     private int user2Id;
+    private Friend.Status status;
 
-    public FriendDTO(int user1Id, int user2Id) {
+    public FriendDTO(int user1Id, int user2Id, Friend.Status status) {
         this.user1Id = user1Id;
         this.user2Id = user2Id;
+        this.status = status;
     }
 
     public FriendDTO(Friend friend) {
         this.user1Id = friend.getUser1().getId();
         this.user2Id = friend.getUser2().getId();
+        this.status = friend.getStatus();
     }
-
     public int getUser1Id() {
         return user1Id;
     }
