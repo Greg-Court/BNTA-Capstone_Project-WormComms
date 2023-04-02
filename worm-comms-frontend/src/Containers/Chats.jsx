89e872c5ada33e894438ffcf9c2dae51fa690a70
Greg <gregcourt10@gmail.com>
2023-04-02 09:58:57 +0100

    chat React-Select working

--- worm-comms-frontend/src/Containers/Chats.jsx
+++ worm-comms-frontend/src/Containers/Chats.jsx
@@ -3,12 +3,13 @@ import { getUserChats, createChat } from "../api";
 import { useCurrentUser } from "../UserContext";
 import Friend from "../Components/Friend";
 import Chat from "../Components/Chat";
-import Select from "react-select"
+import Select from "react-select";
 
 const Chats = () => {
   const { currentUser, setCurrentUser } = useCurrentUser();
   const [chats, setChats] = useState([]);
   const [newChat, setNewChat] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState([]);
 
   useEffect(() => {
     if (currentUser) {
@@ -37,60 +38,50 @@ const Chats = () => {
     }
   };
 
-  const friends = currentUser.friends.map((friend, index) => {
-    return <Friend friend={friend} key={index}></Friend>;
-  });
+  const friendsOptions = currentUser.friends.map((friend) => ({
+    value: friend.user2.username,
+    label: friend.user2.username,
+    data: friend.user2,
+  }));
 
-  const updateNewChat = (e) => {
-    const selectedUsers = Array.from(
-      e.target.selectedOptions,
-      (option) => option.value
-    ).map(
-      (username) =>
-        currentUser.friends.find((friend) => friend.user2.username === username)
-          .user2
-    );
+  const updateNewChat = (selectedOptions) => {
+    const selectedUsers = selectedOptions.map((option) => option.data);
     setNewChat(selectedUsers);
   };
 
   return (
     <div className="h-[85vh]">
       <div className="w-[100%] flex flex-col">
-        <select
-          className="mx-[5%] border-2 max-h-48 overflow-y-auto mt-5"
+        <Select
+          className="mx-[5%] overflow-y-auto mt-3"
+          options={friendsOptions}
           onChange={updateNewChat}
-          multiple
-        >
-          <option className="h-max-5vh py-2" disabled>
-            Contacts:
-          </option>
-          {friends.map((friend) => (
-            <option
-              className="h-max-5vh py-2 hover:bg-blue-200"
-              style={{
-                backgroundColor: friend.selected
-                  ? "rgba(0, 0, 255, 0.1)"
-                  : "transparent",
-              }}
-              key={friend.id}
-            >
-              {friend}
-            </option>
-          ))}
-        </select>
+          isMulti
+          placeholder="Select Contacts..."
+          menuPortalTarget={document.body}
+          styles={{
+            menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+              handleCreateChat();
+            }
+          }}
+        />
         <button
           onClick={handleCreateChat}
-          className="mx-[5%] mt-2 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+          className="mx-[5%] mt-3 mb-1 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
         >
           Create New Chat
         </button>
       </div>
-      <div className="flex items-center justify-around mt-2"></div>
-      <ul className="flex flex-col overflow-y-auto scrollbar-hide h-[70vh]">
+      <div className="flex items-center justify-around"></div>
+      <ul className="flex flex-col overflow-y-auto scrollbar-hide max-h-[70vh]">
         {chats.map((chat) => (
           <div>
-            <Chat key={chat.id} chat={chat} />
             <div className="border mx-[5%] my-2"></div>
+            <Chat key={chat.id} chat={chat} />
           </div>
         ))}
       </ul>
