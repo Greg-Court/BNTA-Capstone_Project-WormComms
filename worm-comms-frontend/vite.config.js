6eddadde7262a3c8d951d81b86c9393d7440073b
Greg <gregcourt10@gmail.com>
2023-04-05 12:48:44 +0100

    added build base URL to relative path

--- worm-comms-frontend/vite.config.js
+++ worm-comms-frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Set the base URL to a relative path
+    base: './',
+  },
 })
