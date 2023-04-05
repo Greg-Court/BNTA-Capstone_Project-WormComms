a1b29e7d6e26f8d795a5b33974922a78a5d48dae
Greg <gregcourt10@gmail.com>
2023-04-05 13:11:32 +0100

    modified API to work locally AND remotely

--- worm-comms-frontend/src/api.jsx
+++ worm-comms-frontend/src/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an instance of axios with a base URL and default headers.
 const apiClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: import.meta.env.VITE_APP_API_BASE_URL || "http://localhost:8080/api",
   headers: {
     "Content-Type": "application/json",
     Authorization: "null",
